61a39c12db61f897a4e22201cb3b4b2684cb5faf
藤田彰子 <akiko@yanMacBook-Air.local>
2025-03-24 12:36:56 +0900

    [update]タグ部分のレイアウトをボタンぽいUIに変更

--- app/src/main/java/com/example/qiitafetcher/ui/ArticleItem.kt
+++ app/src/main/java/com/example/qiitafetcher/ui/ArticleItem.kt
@@ -1,6 +1,7 @@
 package com.example.qiitafetcher.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,11 +20,12 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -202,7 +204,17 @@ private fun Tag(
             .padding(end = 5.dp, bottom = 10.dp)
             .wrapContentWidth()
             .height(20.dp)
-            .background(color = Color.Gray, shape = RoundedCornerShape(2.dp))
+            .shadow(elevation = 1.dp, shape = RoundedCornerShape(2.dp))
+
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.outline,
+                shape = RoundedCornerShape(2.dp)
+            )
+            .background(
+                color = MaterialTheme.colorScheme.secondaryContainer,
+                shape = RoundedCornerShape(2.dp)
+            )
             .padding(2.dp)
             .clickable {
                 onSearch.invoke(tag.name)
@@ -214,7 +226,8 @@ private fun Tag(
         Text(
             text = tag.name,
             style = QFTypography.labelLarge,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onSecondaryContainer
         )
     }
 }
